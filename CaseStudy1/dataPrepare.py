4eaeb2d754a0a3f6d96d7efd974e08239c6767a2
QiuYi111 <2513868362@qq.com>
2025-03-09 19:02:37 +0800

    dataPrepare

--- CaseStudy1/dataPrepare.py
+++ CaseStudy1/dataPrepare.py
@@ -2,27 +2,27 @@ from datasets import load_dataset
 from torchvision.transforms import v2
 import torch
 
-def dataPrepare(dataset_name="mertcobanov/animals",size=(600,600),save_dir=""):
-    dataset=load_dataset(dataset_name,split="train")
-    print("Dataset Length:",len(dataset["image"]))
-    transform=v2.Compose([
-        v2.ToImage(),
-        v2.ToDtype(torch.uint8,scale=True),
-        v2.Resize(size=size),
-        v2.Grayscale(),
-        v2.ToDtype(torch.float32,scale=True),
-        v2.Lambda(lambda x:torch.torch.flatten(x))
-    ]
-    )
-    dataset=dataset.map(lambda x: {"image": transform(x["image"])})
-    all_images=[sample["image"] for sample in dataset]
-    imgTensor=torch.stack(all_images,dim=0)
-    print("Image Set Size: ",imgTensor.shape)
-    dataset.save_to_disk(save_dir+"/dataset")
-    torch.save(imgTensor,save_dir+"/tensor.pt")
-    print("Dataset Prepare Complete! Saved at ",save_dir)
+def dataPrepare(dataset_name="mertcobanov/animals", size=(600,600), save_dir=""):
+    dataset = load_dataset(dataset_name, split="train")
+    transform = v2.Compose([
+        v2.Resize(size),          
+        v2.Grayscale(num_output_channels=1),  
+        v2.ToImage(),             
+        v2.ToDtype(torch.float32, scale=True), 
+    ])
+    
+
+    dataset = dataset.map(lambda x: {"image": transform(x["image"])}, 
+                         batched=True,  
+                         batch_size=128)
+    
+   
+    dataset.save_to_disk(f"{save_dir}/dataset")
+    print(f"Dataset saved at {save_dir}")
+    return
+
 
 
 if __name__=="__main__":
-    dataPrepare(save_dir="./dataProcessed")
+    dataPrepare(save_dir="/Volumes/DataHub/dataProcessed")
 
--- CaseStudy1/svd.py
+++ CaseStudy1/svd.py
@@ -1,5 +1,8 @@
 import torch
-from datasets import load_from_disk
 
 def svd(dataset_dir,):
-    
+    trainset=torch.load(dataset_dir)
+    trainset=trainset-torch.mean(trainset,dim=1,keepdim=True)
+    U, S, Vh = torch.linalg.svd(trainset,False)
+    torch.save(U,dataset_dir+"U.pt")
+
