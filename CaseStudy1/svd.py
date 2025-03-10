76ab88975924db83af26d03fc7fb97c10a0084f3
QiuYi111 <2513868362@qq.com>
2025-03-10 18:27:02 +0800

    svd

--- CaseStudy1/svd.py
+++ CaseStudy1/svd.py
@@ -1,4 +1,20 @@
 import torch
 from datasets import load_from_disk
 
-dataset=load_from_disk("/Volumes/DataHub/")
+dataset=load_from_disk("/Volumes/DataHub/dataProcessed/datasetSVD")
+
+print("Dataset Loaded from disk")
+
+X=[torch.reshape(sample["image"],(36000,)) for sample in dataset]
+
+torch.stack(X)
+
+print("X matrix built")
+
+U,S,Vh=torch.linalg.svd(X)
+
+print("SVD Completed!")
+
+torch.save(U,"/Volumes/DataHub/dataProcessed/eigenMatrix.pt")
+
+print("U matrix Saved!")
